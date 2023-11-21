8850c1303fde1622902ef364ebe00a8703d8bfa3
Oleh Dubishchev <Oleh.Dubishchev@infopulse.com>
2023-11-21 12:27:45 +0200

    delete

--- Google/UnitTest1.cs
+++ Google/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Allure.Commons;
 using NUnit.Allure.Attributes;
 using NUnit.Allure.Core;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using WebDriverManager;
